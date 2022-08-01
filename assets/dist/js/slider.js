e2dc0e815eb5b95056dc9b998c6df35daf751aad
adhasanblog <fhasanblog@gmail.com>
2022-08-01 20:34:22 +0700

    resto v1.1

--- assets/dist/js/slider.js
+++ assets/dist/js/slider.js
@@ -1,24 +1,24 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    spaceBetween: 40,
-    width: 1128,
-    direction: 'horizontal',
-    loop: true,
-    loopedSlides: 5,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  spaceBetween: 40,
+  width: 1128,
+  direction: "horizontal",
+  loop: true,
+  loopedSlides: 3,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
